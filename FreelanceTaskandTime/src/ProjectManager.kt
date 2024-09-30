5652bb24ca7e8b2cb183f37fde02d6ee74a90410
taufikadip <77040564+taufikadip@users.noreply.github.com>
2024-09-30 14:37:54 +0700

    Change property Budget to var

--- FreelanceTaskandTime/src/ProjectManager.kt
+++ FreelanceTaskandTime/src/ProjectManager.kt
@@ -1,2 +1,9 @@
 class ProjectManager {
+
+    val freelancers = mutableListOf<Freelancer>()
+    val projects = mutableListOf<Project>()
+
+//    fun calculateBudget() {
+//        totalBudget = tasks.sumByDouble { it.actualHours * (it.freelancer?.hourlyRate ?: 0.0) }
+//    }
 }
