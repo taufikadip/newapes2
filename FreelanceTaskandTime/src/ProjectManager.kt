9b7b233f68b927918cb731570c07b979c21ed47a
I Dewa Gede Ngr Bramasta D <68767@fif.co.id>
2024-09-30 14:31:32 +0700

    Function1&2

--- FreelanceTaskandTime/src/ProjectManager.kt
+++ FreelanceTaskandTime/src/ProjectManager.kt
@@ -1,2 +1,25 @@
+import java.time.LocalDate
+
 class ProjectManager {
+    private val freelancers = mutableListOf<Freelancer>()
+    private val projects = mutableListOf<Project>()
+
+    fun addFreelancer(name: String, hourlyRate: Double) {
+        val freelancer = Freelancer(
+            id = freelancers.size + 1,
+            name = name,
+            hourlyRate = hourlyRate
+        )
+        freelancers.add(freelancer)
+    }
+
+    fun createProject(name: String, client: String, deadline: LocalDate) {
+        val project = Project(
+            id = projects.size + 1,
+            name = name,
+            client = client,
+            deadline = deadline
+        )
+        projects.add(project)
+    }
 }
