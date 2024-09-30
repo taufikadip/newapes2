ed3d871eed3a8572b96b3ca57410a608e7d8c56e
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-30 14:18:25 +0700

    entity task dan freelancer

--- FreelanceTaskandTime/src/Freelancer.kt
+++ FreelanceTaskandTime/src/Freelancer.kt
@@ -1,2 +1,11 @@
-class Freelancer {
+data class Freelancer(
+    val id: Int,
+    val name: String,
+    val hourlyRate: Double,
+    val assignedTasks: MutableList<Task> = mutableListOf()
+) {
+    init {
+        require(name.isNotEmpty()) { "Nama tidak boleh kosong" }
+        require(hourlyRate > 0) { "Estimasi waktu harus bernilai positif" }
+    }
 }
--- FreelanceTaskandTime/src/Task.kt
+++ FreelanceTaskandTime/src/Task.kt
@@ -1,2 +1,23 @@
-class Task {
-}
+data class Task (
+    val id: Int,
+    var description: String,
+    var freelancer: Freelancer? = null,
+    val estimatedHours: Int,
+    var actualHours: Int = 0,
+    var status: String = "Not Started"
+    ) {
+        init {
+            require(description.isNotEmpty()) { "Deskripsi tidak boleh kosong" }
+            require(estimatedHours > 0) { "Estimasi waktu harus bernilai positif" }
+        }
+
+        fun logHours(hours: Int) {
+            require(status != "Completed") { "Tidak bisa menambahkan jam kepada Task yang selesai" }
+            actualHours += hours
+            if (actualHours >= estimatedHours) {
+                status = "Completed"
+            } else {
+                status = "In Progress"
+            }
+        }
+    }
